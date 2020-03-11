6280cb020694e16f7deb4413467d76af021b6732
GagikNav <60577739+GagikNav@users.noreply.github.com>
2020-03-11 18:39:47 +0100

    Back to top button added
    button created with js  and added some styles in css file.
    three main functions created
    . create button
    . scroll
    . hide button if scroll is on top

--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -28,6 +28,38 @@ let navbar = document.getElementById('navbar__list');
 
 /* Start Helper Functions
  */
+
+//$ write a function that makes a button witch
+//$ on click it will jump to top!
+
+const createButton = () => {
+	// dom element for button
+	let button = document.createElement('button');
+	button.innerHTML = 'Go To Top';
+	button.classList = 'btn';
+	button.id = 'btn';
+	button.setAttribute('onclick', 'goToTop()');
+	document.body.appendChild(button);
+};
+
+//$ this function after clicking the button scrolls to the top
+
+const goToTop = () => {
+	window.scroll({
+		top: 0,
+		behavior: 'smooth'
+		/* this peace of code smooths the scrolling with css
+	// this function adds a class to the <HTML> tag then scrolls top after that removes class
+	//  in order to smooth scrolling
+	// htmlElement.className = 'smoothScroll';
+	//document.body.scrollTop = 0;
+	// document.documentElement.scrollTop = 0;
+	// htmlElement.classList.remove('smoothScroll');
+	*/
+	});
+};
+
+//$ this function adds "li" an "a" correspond to each section
 let addNavbar = () => {
 	for (let section of sections) {
 		const navbarItems = document.createElement('li');
@@ -40,27 +72,40 @@ let addNavbar = () => {
 		navbarItems.appendChild(navbarLinks);
 	}
 };
-function navClick() {
-	let navbarLinks = document.getElementsByClassName('menu__link');
-	navbarLinks.addEventListener('click', function(e) {
-		const goTo = document.querySelector('#' + e.target.section.id);
-		// goTo.scrollIntoView({ behavior: 'smooth' });
-	});
-}
 
-let clickLink = () => {};
-//
 /**
  * End Helper Functions
+ *
+ *
  * Begin Main Functions
  *
  */
 
-//
+//$ Function for clicking each navBar Link with a listener of class
 
-//* Add class 'active' to section when near top of viewport
+function navBarClick() {
+	link = document.querySelectorAll('.menu__link');
 
-// Scroll to anchor ID using scrollTO event
+	//finding each link for click and listen to it
+	for (let i = 0; i < link.length; i++) {
+		link[i].addEventListener('click', function(e) {
+			e.preventDefault();
+			//defining variable for each section name
+			//? it has a limitation of names to "Section + number" =================> should review again!!!
+			let secnum = i + 1;
+			let sc = document.getElementById('section' + secnum);
+
+			//defining scroll to the designated section
+			// And there is a timeout function present for better experience
+			setTimeout(() => {
+				sc.scrollIntoView({
+					block: 'start',
+					behavior: 'smooth'
+				});
+			}, 150);
+		});
+	}
+}
 
 /**
  * End Main Functions
@@ -69,9 +114,23 @@ let clickLink = () => {};
  */
 
 // Build menu
-addNavbar();
 //Adds each section to the navbar
+addNavbar();
 
+// Scroll to anchor ID using scrollTO event
 // Scroll to section on link click
-navClick();
+navBarClick();
+
 // Set sections as active
+//* Add class 'active' to section when near top of viewport
+
+//create Button and on click calls goToTop function
+createButton();
+
+window.onscroll = () => {
+	if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+		document.getElementById('btn').style.display = 'block';
+	} else {
+		document.getElementById('btn').style.display = 'none';
+	}
+};
