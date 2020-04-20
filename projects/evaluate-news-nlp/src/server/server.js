165626e8bcda799952101e4067949ae1e9b72cfb
GagikNav <60577739+GagikNav@users.noreply.github.com>
2020-04-20 20:16:34 +0200

    port edited

--- projects/evaluate-news-nlp/src/server/server.js
+++ projects/evaluate-news-nlp/src/server/server.js
@@ -23,7 +23,7 @@ const textapi = new aylien({
    application_key: process.env.API_KEY,
 });
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
    console.log(`Listening on localhost port ${port}........\n\n`);
 });
